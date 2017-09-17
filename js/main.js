001742f48398879757f80db7f4f5700b7227e4ba
Michal Stabrawa <mistaaqua@gmail.com>
2017-09-17 21:12:15 +0200

    Tropidog website zoogo

---
+++ js/main.js
@@ -0,0 +1,37 @@
+$(function () {
+    var okno = $(window);
+    var menu = $('#main-nav');
+
+   if (okno.scrollTop() >= 100) {
+      menu.addClass('scroll');
+   }
+
+   okno.scroll(function () {
+
+      if (okno.scrollTop() >= 100) {
+            menu.addClass('scroll');
+       } else {
+            menu.removeClass('scroll');
+        }
+  });
+
+    /*Smoth scroll*/
+
+    $(document).on('click', 'a[href^="#"]', function(event) {
+       event.preventDefault();
+var menuHeight = $('#main-nav').height();
+       $('html, body').animate({
+            scrollTop: $($.attr(this, 'href')).offset().top - menuHeight
+        }, 500);
+  });
+
+
+});
+
+/*Toogle-------------------------------------------------------------------------------------------------------------- */
+$(document).ready(function(){
+    $(".btn").click(function(){
+        $("#div1").toggle(1000);
+    });
+});
+
