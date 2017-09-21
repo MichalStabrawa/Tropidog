1842b3865b37d19b643d8a2c59b56158640c095e
Michal Stabrawa <mistaaqua@gmail.com>
2017-09-21 15:42:15 +0200

    btn jQuery

--- js/main.js
+++ js/main.js
@@ -2,50 +2,50 @@ $(function () {
     var okno = $(window);
     var menu = $('#main-nav');
 
-   if (okno.scrollTop() >= 100) {
-      menu.addClass('scroll');
-   }
+    if (okno.scrollTop() >= 100) {
+        menu.addClass('scroll');
+    }
 
-   okno.scroll(function () {
+    okno.scroll(function () {
 
-      if (okno.scrollTop() >= 100) {
+        if (okno.scrollTop() >= 100) {
             menu.addClass('scroll');
-       } else {
+        } else {
             menu.removeClass('scroll');
         }
-  });
+    });
 
     /*Smoth scroll*/
 
-    $(document).on('click', 'a[href^="#"]', function(event) {
-       event.preventDefault();
-var menuHeight = $('#main-nav').height();
-       $('html, body').animate({
+    $(document).on('click', 'a[href^="#"]', function (event) {
+        event.preventDefault();
+        var menuHeight = $('#main-nav').height();
+        $('html, body').animate({
             scrollTop: $($.attr(this, 'href')).offset().top - menuHeight
         }, 500);
-  });
+    });
 
 
 });
 
 /*Toogle-------------------------------------------------------------------------------------------------------------- */
-$(document).ready(function(){
-    $(".btn").click(function(){
+$(document).ready(function () {
+    $(".btn").click(function () {
         $("#div1").toggle(1000);
     });
 });
 
 
 /*Toogle TropidogSuper-content*/
-$(document).ready(function(){
-    $("#btn-tropidog1").click(function(){
+$(document).ready(function () {
+    $("#btn-tropidog1").click(function () {
         $("#tropidogSuper-content").slideToggle("slow");
     });
 });
 
 
-$(document).ready(function(){
-    $("#btn1-tropidog2").click(function(){
+$(document).ready(function () {
+    $("#btn1-tropidog2").click(function () {
         $("#tropidogPremium-content").slideToggle("slow");
     });
 });
